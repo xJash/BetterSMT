2a2654357dc30d14ccaa85810620d2c76f40819c
xJash <ZackMitchell1211@live.com>
2024-11-09 21:33:32 -0500

    Initial push

---
+++ Patches/Notificaitonhandler.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+
+namespace BetterSMT.Patches;
+
+
+[HarmonyPatch(typeof(GameCanvas))]
+internal class NotificationHandler
+{
+    [HarmonyPatch("Update")]
+    [HarmonyPostfix]
+    public static void NotificationHandler_Postfix(GameCanvas __instance, ref bool ___inCooldown)
+    {
+        if (BetterSMT.notify)
+        {
+            ___inCooldown = false;
+            BetterSMT.notify = false;
+            string Notification = "`";
+            switch (BetterSMT.notificationType)
+            {
+                case "priceToggle":
+                    Notification += "Double Price: " + (BetterSMT.doublePrice ? "ON" : "OFF");
+                    break;
+                case "roundDownSwitch":
+                    Notification += "Roudning to nearest " + (BetterSMT.NearestTen.Value ? "ten" : "five") + (!BetterSMT.roundDown.Value ? "\r\n(Currently disabled)" : "");
+                    break;
+                case "roundDownToggle":
+                    Notification += "Rounding has been " + (BetterSMT.roundDown.Value ? "enabled" : "disabled");
+                    break;
+                default:
+                    break;
+            }
+            __instance.CreateCanvasNotification(Notification);
+        }
+    }
+}
--- Patches/UpgradesManagerPatch.cs
+++ Patches/UpgradesManagerPatch.cs
@@ -11,26 +11,26 @@ public class UpgradesManagerPatch
 	{
 		switch (perkIndex)
 		{
-			case 0:
-				NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
-				NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
-				break;
-			case 1:
-				NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
-				NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
-				break;
-			case 2:
-				NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
-				NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
-				break;
-			case 3:
-				NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
-				NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
-				break;
-			case 4:
-				NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
-				NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
-				break;
+			//case 0:
+			//	NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
+			//	NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
+			//	break;
+			//case 1:
+			//	NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
+			//	NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
+			//	break;
+			//case 2:
+			//	NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
+			//	NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
+			//	break;
+			//case 3:
+			//	NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
+			//	NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
+			//	break;
+			//case 4:
+			//	NPC_Manager.Instance.maxEmployees += BetterSMT.EmployeesPerPerk.Value;
+			//	NPC_Manager.Instance.UpdateEmployeesNumberInBlackboard();
+			//	break;
             case 5:
                 NPC_Manager.Instance.extraEmployeeSpeedFactor += BetterSMT.EmployeeSpeedPerPerk.Value;
                 NPC_Manager.Instance.UpdateEmployeeStats();
@@ -67,12 +67,15 @@ public class UpgradesManagerPatch
                 break;
             case 16:
                 NPC_Manager.Instance.productCheckoutWait -= BetterSMT.EmployeeCheckoutPerPerk1.Value;
+                Debug.Log("ProductCheckoutWait after perk 1: " + NPC_Manager.Instance.productCheckoutWait);
                 break;
             case 17:
                 NPC_Manager.Instance.productCheckoutWait -= BetterSMT.EmployeeCheckoutPerPerk2.Value;
+                Debug.Log("ProductCheckoutWait after perk 2: " + NPC_Manager.Instance.productCheckoutWait);
                 break;
             case 18:
                 NPC_Manager.Instance.productCheckoutWait -= BetterSMT.EmployeeCheckoutPerPerk3.Value;
+                Debug.Log("ProductCheckoutWait after perk 3: " + NPC_Manager.Instance.productCheckoutWait);
                 break;
             case 19:
                 NPC_Manager.Instance.employeeItemPlaceWait -= BetterSMT.EmployeeRestockPerPerk.Value;
