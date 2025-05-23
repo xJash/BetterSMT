0eb8027731b8ec96f4719dd245877b6a197e14b6
Mitch <Mitch@DESKTOP-716IGE0>
2025-05-23 09:40:36 -0400

    _

--- Patches/OrderingDevicePatch.cs
+++ Patches/OrderingDevicePatch.cs
@@ -1,4 +1,7 @@
 ﻿using HarmonyLib;
+using System.Collections;
+using System.Globalization;
+using TMPro;
 using UnityEngine;
 
 namespace BetterSMT.Patches;
@@ -13,6 +16,10 @@ public static class OrderingDevicePatch {
             ProductListing listing = ProductListing.Instance;
             GameObject shelves = NPC_Manager.Instance?.shelvesOBJ;
 
+            if (manager == null || listing == null || shelves == null) {
+                return;
+            }
+
             int addedTotal = 0;
 
             for (int productId = 0; productId < listing.productPrefabs.Length; productId++) {
@@ -26,7 +33,7 @@ public static class OrderingDevicePatch {
                     int[] array = container.productInfoArray;
                     for (int j = 0; j < array.Length / 2; j++) {
                         int id = array[j * 2];
-                        int quantity = array[j * 2 + 1];
+                        int quantity = array[(j * 2) + 1];
                         if (id == productId && quantity >= 0) {
                             int maxPerRow = NPC_Manager.Instance.GetMaxProductsPerRow(i, productId);
                             missingItems += maxPerRow - quantity;
@@ -34,14 +41,32 @@ public static class OrderingDevicePatch {
                     }
                 }
 
-                int boxesNeeded = Mathf.CeilToInt((float)missingItems / maxItemsPerBox);
+                float rawBoxesNeeded = (float)missingItems / maxItemsPerBox;
+
+                // ✅ Subtract boxes already in store, using reflection to access `boxesInStoreField`
+                float boxesInStore = 0f;
+                System.Reflection.FieldInfo fieldInfo = AccessTools.Field(typeof(OrderingDevice), "boxesInStoreField");
+                if (fieldInfo != null) {
+                    TMP_Text textField = fieldInfo.GetValue(__instance) as TMP_Text;
+                    if (textField != null) {
+                        string rawText = textField.text.Replace("x", "").Replace(",", ".");
+                        if (float.TryParse(rawText, out float parsed)) {
+                            boxesInStore = parsed;
+                        }
+                    }
+                }
+
+                float netBoxesNeeded = Mathf.Max(0, rawBoxesNeeded - boxesInStore);
+                int finalBoxesToOrder = Mathf.CeilToInt(netBoxesNeeded);
                 float pricePerBox = ProductPriceUtility.GetPricePerBox(productId);
 
-                for (int b = 0; b < boxesNeeded; b++) {
+                for (int b = 0; b < finalBoxesToOrder; b++) {
                     manager.AddShoppingListProduct(productId, pricePerBox);
                     addedTotal++;
                 }
             }
+
+            Debug.Log($"[BetterSMT] Auto-ordering complete. {addedTotal} boxes added to cart.");
         }
     }
 
@@ -59,4 +84,75 @@ public static class OrderingDevicePatch {
             return Mathf.Round(raw * 100f) / 100f;
         }
     }
+
+    [HarmonyPatch("ClearProduct")]
+    [HarmonyPrefix]
+    public static void ClearProductPatch(OrderingDevice __instance, int productID) {
+        if (__instance.listParentOBJ.transform.childCount == 0) {
+            return;
+        }
+        for (int i = 0; i < __instance.listParentOBJ.transform.childCount; i++) {
+            Transform child = __instance.listParentOBJ.transform.GetChild(i);
+            if (child.GetComponent<OrderingListReferences>().productID == productID) {
+                Object.Destroy(child.gameObject);
+                __instance.stylusAnimator.SetFloat("AnimationFactor", 1f);
+                __instance.stylusAnimator.Play("StylusAnimation");
+                __instance.StartCoroutine(__instance.AnimationHighlight(1));
+                int childCount = GameData.Instance.GetComponent<ManagerBlackboard>().shoppingListParent.transform.childCount;
+                if (i < childCount) {
+                    int indexToRemove = childCount - 1 - i;
+                    GameData.Instance.GetComponent<ManagerBlackboard>().RemoveShoppingListProduct(indexToRemove);
+                }
+                break;
+            }
+        }
+        __instance.StartCoroutine(__instance.SetInListField(productID));
+        _ = __instance.StartCoroutine(CalculateShoppingListTotalOverrideOrdering(__instance));
+    }
+
+    public static IEnumerator CalculateShoppingListTotalOverrideOrdering(OrderingDevice __instance) {
+        GameObject managerListParentOBJ = GameData.Instance.GetComponent<ManagerBlackboard>().shoppingListParent;
+        yield return new WaitForEndOfFrame();
+
+        __instance.totalBoxesAmount.text = "x" + __instance.listParentOBJ.transform.childCount;
+        float num = 0f;
+
+        if (managerListParentOBJ.transform.childCount > 0) {
+            foreach (Transform item in managerListParentOBJ.transform) {
+                string text = item.transform.Find("BoxPrice").GetComponent<TextMeshProUGUI>().text;
+                string cleanedText = text[2..].Trim();
+
+                if (float.TryParse(cleanedText, NumberStyles.Float, CultureInfo.InvariantCulture, out float price)) {
+                    num += price;
+                } else {
+                    Debug.LogWarning($"BetterSMT: Failed to parse float from price string '{cleanedText}'");
+                }
+            }
+        }
+
+        string text2 = ProductListing.Instance.ConvertFloatToTextPrice(num);
+        __instance.totalPriceField.text = text2;
+    }
+
+    [HarmonyPatch("CopyManagerBlackboardList")]
+    [HarmonyPrefix]
+    public static void CopyManagerBlackboardListPatch(OrderingDevice __instance) {
+        GameObject shoppingListParent = GameData.Instance.GetComponent<ManagerBlackboard>().shoppingListParent;
+        for (int i = 0; i < shoppingListParent.transform.childCount; i++) {
+            int thisSkillIndex = shoppingListParent.transform.GetChild(i).GetComponent<InteractableData>().thisSkillIndex;
+            __instance.AddProductbase(thisSkillIndex);
+        }
+        _ = __instance.StartCoroutine(CalculateShoppingListTotalOverrideOrdering(__instance));
+    }
+
+    [HarmonyPatch("AddProduct")]
+    [HarmonyPrefix]
+    public static void AddProductPatch(OrderingDevice __instance, int productID) {
+        __instance.AddProductbase(productID);
+        float boxPrice = __instance.RetrievePricePerBox(productID);
+        GameData.Instance.GetComponent<ManagerBlackboard>().AddShoppingListProduct(productID, boxPrice);
+        __instance.StartCoroutine(__instance.SetInListField(productID));
+        _ = __instance.StartCoroutine(CalculateShoppingListTotalOverrideOrdering(__instance));
+    }
+
 }
