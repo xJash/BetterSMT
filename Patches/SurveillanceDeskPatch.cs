928ccffcfef1f72a291f051972aea1c8a3d9cfd7
Mitche <Mitche@DESKTOP-F16HUJH>
2025-08-12 12:20:47 -0400

    - OneClickCheckMark surveillance feature fixed

--- Patches/SurveillanceDeskPatch.cs
+++ Patches/SurveillanceDeskPatch.cs
@@ -13,9 +13,7 @@ namespace BetterSMT.Patches
             if (BetterSMT.OneClickCheckMark.Value)
             {
                 if (!__instance.nSpawner || !__instance.inASecurityCamera)
-                {
                     return false;
-                }
 
                 if (__instance.MainPlayer.GetButtonDown("Build"))
                 {
@@ -44,74 +42,30 @@ namespace BetterSMT.Patches
                     __instance.y -= __instance.MainPlayer.GetAxis("MoveV") * __instance.baseYSpeed * 6f * 0.02f;
                     __instance.y = SurveillanceDesk.ClampAngle(__instance.y, __instance.yMinLimit, __instance.yMaxLimit);
                     __instance.x = SurveillanceDesk.ClampAngle(__instance.x, __instance.xMinLimit, __instance.xMaxLimit);
-                    Quaternion localRotation = Quaternion.Euler(__instance.y, __instance.x, 0f);
-                    __instance.viewpointOBJ.transform.localRotation = localRotation;
+                    __instance.viewpointOBJ.transform.localRotation = Quaternion.Euler(__instance.y, __instance.x, 0f);
                 }
 
                 if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out var hitInfo, 100f, __instance.lMask)
                     && hitInfo.transform.gameObject.CompareTag("Interactable")
                     && hitInfo.transform.parent
                     && hitInfo.transform.parent.GetComponent<NPC_Info>()
-                    && __instance.MainPlayer.GetButton("Main Action"))
+                    && __instance.MainPlayer.GetButtonDown("Main Action"))
                 {
-                    NPC_Info component = hitInfo.transform.parent.GetComponent<NPC_Info>();
-                    if (!hitInfo.transform.parent.Find("3DTick") && component.isCustomer)
-                    {
-                        if (component.surveillanceChecked)
-                        {
-                            GameCanvas.Instance.CreateCanvasNotification("surveillance1");
-                            if (!hitInfo.transform.parent.Find("3DTick"))
-                            {
-                                __instance.Create3DTickObject(hitInfo.transform.parent);
-                            }
-                        }
-                        else if (__instance.currentCustomerOBJ && hitInfo.transform.parent.gameObject == __instance.currentCustomerOBJ)
-                        {
-                            float num = Vector3.Distance(__instance.currentCustomerOBJ.transform.position, Camera.main.transform.position);
-                            float num2;
-                            if (true)
-                            {
-                                float t = 1f / (num - 4f);
-                                num2 = Mathf.Lerp(__instance.fillMinAmountPerClick, __instance.fillMaxAmountPerClick, t);
-                            }
+                    NPC_Info npc = hitInfo.transform.parent.GetComponent<NPC_Info>();
 
-                            __instance.currentFillScale += num2 * Time.deltaTime * 3f;
-                            float pitch = 1f + num2 / 10f;
-                            __instance.clickBeepAudioSource.pitch = pitch;
-                            __instance.clickBeepAudioSource.Play();
-                        }
-                        else if (!__instance.MainPlayer.GetButtonPrev("Main Action") || !__instance.currentCustomerOBJ)
-                        {
-                            __instance.currentCustomerOBJ = hitInfo.transform.parent.gameObject;
-                            __instance.currentFillScale = 0f;
-                        }
-                    }
-                }
-
-                if (__instance.currentCustomerOBJ)
-                {
-                    __instance.spawnedCylinderOBJ.transform.position = __instance.currentCustomerOBJ.transform.position;
-                    if (__instance.currentFillScale > 0.99f)
+                    if (!hitInfo.transform.parent.Find("3DTick") && npc.isCustomer)
                     {
-                        __instance.cylinderFillOBJ.transform.localScale = new Vector3(1f, 1f, 1f);
-                        __instance.currentCustomerOBJ.GetComponent<NPC_Info>().surveillanceChecked = true;
-                        __instance.currentCustomerOBJ.GetComponent<NPC_Info>().CmdSurveillanceSet();
+                        npc.surveillanceChecked = true;
+                        npc.CmdSurveillanceSet();
                         __instance.surveillanceFinishAudioSource.Play();
-                        __instance.Create3DTickObject(__instance.currentCustomerOBJ.transform);
-                        __instance.currentCustomerOBJ = null;
-                    }
-                    else
-                    {
-                        __instance.currentFillScale -= __instance.unfillAmount * Time.deltaTime;
-                        __instance.currentFillScale = Mathf.Clamp(__instance.currentFillScale, 0f, 1f);
-                        __instance.cylinderFillOBJ.transform.localScale = new Vector3(1f, __instance.currentFillScale, 1f);
+                        __instance.Create3DTickObject(hitInfo.transform.parent);
+                        GameCanvas.Instance.CreateCanvasNotification("surveillance1");
                     }
                 }
-                else
-                {
-                    __instance.spawnedCylinderOBJ.transform.position = new Vector3(0f, -10f, 0f);
-                    __instance.cylinderFillOBJ.transform.localScale = new Vector3(1f, 0f, 1f);
-                }
+
+                __instance.currentCustomerOBJ = null;
+                __instance.spawnedCylinderOBJ.transform.position = new Vector3(0f, -10f, 0f);
+                __instance.cylinderFillOBJ.transform.localScale = new Vector3(1f, 0f, 1f);
 
                 return false;
             }
