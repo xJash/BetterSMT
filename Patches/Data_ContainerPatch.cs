29bb238a11b01d38d0e3f9a08cccc80feb1643ed
Mitche <Mitche@DESKTOP-F16HUJH>
2025-09-03 14:52:10 -0400

    - fixed checks for quickstocking/pallet products/no product requirements - fixed check for quickstocking to be able to be disabled

--- Patches/Data_ContainerPatch.cs
+++ Patches/Data_ContainerPatch.cs
@@ -10,36 +10,43 @@ public class Patch_Data_Container {
 
     [HarmonyPatch("BreakingEvent"), HarmonyPostfix]
     public static void BreakingEvent(Data_Container __instance) {
-        if(BetterSMT.SelfCheckoutBreak?.Value == true)
+        if(BetterSMT.SelfCheckoutBreak?.Value == true) {
             __instance.isBroken = false;
+        }
     }
 
     [HarmonyPatch("AddItemToRow"), HarmonyPrefix]
     private static bool AddItemToRowPatch(Data_Container __instance,int containerNumber,int productIDToAdd) {
-        if(!BetterSMT.QuickStocking.Value || !BetterSMT.PalletProduct.Value || !BetterSMT.AllProduct.Value)
+        // Only skip if all three are disabled
+        if(!BetterSMT.QuickStocking.Value && !BetterSMT.PalletProduct.Value && !BetterSMT.AllProduct.Value) {
             return true;
+        }
 
-        var player = FirstPersonController.Instance;
-        var permissions = player.GetComponent<PlayerPermissions>();
-        if(!permissions.RequestRP())
+        FirstPersonController player = FirstPersonController.Instance;
+        PlayerPermissions permissions = player.GetComponent<PlayerPermissions>();
+        if(!permissions.RequestRP()) {
             return false;
+        }
 
         bool highLatency = !__instance.isServer && FsmVariables.GlobalVariables.FindFsmBool("HighLatencyMode").Value;
-        if(highLatency && __instance.highLatencyCooldown)
+        if(highLatency && __instance.highLatencyCooldown) {
             return false;
+        }
 
         __instance.productlistComponent ??= GameData.Instance.GetComponent<ProductListing>();
 
         GameObject prefab = __instance.productlistComponent.productPrefabs[productIDToAdd];
-        var dataProduct = prefab.GetComponent<Data_Product>();
-        var box = prefab.GetComponent<BoxCollider>();
+        Data_Product dataProduct = prefab.GetComponent<Data_Product>();
+        BoxCollider box = prefab.GetComponent<BoxCollider>();
         Vector3 size = box.size;
 
+        // Pallet check
         if(!BetterSMT.PalletProduct.Value && __instance.isVolumeRestricted && size.x * size.y * size.z < __instance.productVolumeLimit) {
             GameCanvas.Instance.CreateCanvasNotification("message20");
             return false;
         }
 
+        // Shelf type check
         if(!BetterSMT.AllProduct.Value && dataProduct.productContainerClass != __instance.containerClass) {
             GameCanvas.Instance.CreateCanvasNotification("message0");
             return false;
@@ -68,27 +75,41 @@ public class Patch_Data_Container {
             return false;
         }
 
-        var network = player.GetComponent<PlayerNetwork>();
+        PlayerNetwork network = player.GetComponent<PlayerNetwork>();
         int itemsAvailable = network.extraParameter2;
         int shelfSpaceLeft = capacity - currentCount;
-        int itemsToAdd = Mathf.Min(itemsAvailable,shelfSpaceLeft);
+
+        int itemsToAdd;
+
+        if(BetterSMT.QuickStocking.Value) {
+            itemsToAdd = Mathf.Min(itemsAvailable,shelfSpaceLeft);
+        } else {
+            if(highLatency) {
+                _ = __instance.StartCoroutine(__instance.HighLatencyCoroutine());
+
+                int spaceClamp = Mathf.Clamp(shelfSpaceLeft,1,5);
+                int availClamp = Mathf.Clamp(itemsAvailable,1,5);
+                itemsToAdd = Mathf.Min(spaceClamp,availClamp);
+            } else {
+                itemsToAdd = (itemsAvailable > 0 && shelfSpaceLeft > 0) ? 1 : 0;
+            }
+        }
 
         if(itemsToAdd <= 0) {
             GameCanvas.Instance.CreateCanvasNotification("message1");
             return false;
         }
 
-        if(highLatency)
-            _ = __instance.StartCoroutine(__instance.HighLatencyCoroutine());
-
         network.extraParameter2 -= itemsToAdd;
         currentCount += itemsToAdd;
-        AchievementsManager.Instance.CmdAddAchievementPoint(1,itemsToAdd);
 
+        AchievementsManager.Instance.CmdAddAchievementPoint(1,itemsToAdd);
         GameData.Instance.PlayPopSound();
+
         __instance.productInfoArray[index + 1] = currentCount;
         __instance.CmdUpdateArrayValues(index,productIDToAdd,currentCount);
 
-        return false; 
+        return false;
     }
+
 }
